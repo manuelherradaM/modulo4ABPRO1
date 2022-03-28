ba21925d19d69bcb81b87603cd296a5dd7a1c5e5
manuel <manuel.musri@gmail.com>
2022-03-28 12:35:45 -0300

    funciones de js 28/03

---
+++ dist/javascript/scripts.js
@@ -0,0 +1,81 @@
+$(function(){
+    $("#nav-placeholder").load("nav.html")
+  })
+
+
+  $(function(){
+    $("#footer-placeholder").load("footer.html")
+  })
+
+   
+
+ function solicitud(){
+  
+    var datos= document.querySelector(".imprimirNombre");
+    let nombre;
+    nombre=prompt("Escriba su nombre")
+    datos.innerHTML=("Nombre: " + " " + nombre)
+
+    var datos= document.querySelector(".imprimirApellido");
+    let apellido;
+    apellido=prompt("Escriba su apellido")
+    datos.innerHTML=("Apellido: " + " " + apellido)  
+ }
+
+
+
+
+function tiempo(){
+  
+  setTimeout(solicitud, 1000);
+}
+/**let namePerson; 
+console.log(namePerson);
+namePerson=prompt("Escriba su nombre");
+console.log(namePerson);
+
+let lastname;
+lastname=prompt("Escriba su apellido")
+console.log(lastname)
+
+let nombre;
+let apellido;
+const contador=[1,2,4,6,"Día"]
+
+function solicitud() {
+    x=prompt("nombre o apellido");
+    return x
+}
+nombre=solicitud()
+apellido=solicitud()
+console.log(`mi nombre es ${nombre}, y mi apellido es ${apellido}`)
+contador.push(nombre)
+console.log(contador)
+
+//
+let numero=1;
+
+//est muestra el proceso
+for (let i=0; i<10; i++) {
+console.log(numero)
+console.log(`i=${i}`)
+numero++
+}
+
+
+//esto 
+let dia
+const semana=[]
+dia=prompt("Cuátos días tiene la semana?")
+for (let i=0; i<dia; i++) {
+    //semana[i]=prompt("agregue un día")
+    semana.push(prompt("agregue un dia")) [i]
+}
+console.log(semana)
+semana
+
+
+setTimeout(()=>{
+        nombreDeLaFuncion();
+    },1000);
+*/
